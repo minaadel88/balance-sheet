de2d06db3e871df1aa252185850818faf68206c3
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2025-02-14 11:10:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    hmr: {
-      timeout: 60000
-    }
-  }
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
